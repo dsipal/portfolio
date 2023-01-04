c36b64efb2b10d35fb0f1f427636e58fed5d8466
Devin Sipal <devin@devinsipal.com>
2023-01-04 22:50:10 +0000

    remove logs

--- components/posts.js
+++ components/posts.js
@@ -5,8 +5,6 @@ const Posts = ({ posts }) => {
   const leftPostsCount = Math.ceil(posts.length / 5);
   const leftPosts = posts.slice(0, leftPostsCount);
   const rightPosts = posts.slice(leftPostsCount, posts.length);
-  console.log(leftPostsCount)
-  console.log(rightPosts)
   return (
     <div>
       <div className="uk-child-width-1-2@s" data-uk-grid="true">
