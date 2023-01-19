5d939d438ee1ea99eafaf30a6fab489b2efb306f
Devin Sipal <devin@devinsipal.com>
2023-01-19 22:56:43 +0000

    update about page

--- pages/about.js
+++ pages/about.js
@@ -6,47 +6,91 @@ import { fetchAPI } from "../lib/api";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from 'rehype-raw';
 import Link from "next/link";
-import { FaGithubAlt,FaLinkedin } from 'react-icons/fa';
+import { FaGithubAlt,FaLinkedin,FaFileWord } from 'react-icons/fa';
 
 
 const About = ({ about, categories}) => {
-    return (
-      <Layout categories={categories}>
-        <div
-          id="container"
-          className="max-w-7xl text-center justify-center items-center mx-auto mt-10 text-lg"
-        >
-          <Image
-            image={about.attributes.image}
-            className="mx-auto mt-20"
-            alt="a picture of me"
-          />
+  return (
+    <Layout categories={categories}>
+      <div
+        id="container"
+        className="max-w-5xl text-center justify-center 
+        items-center mx-auto mt-10 text-lg
+        grid sm:grid-cols-2 grid-cols-1 gap-x-4 p-5"
+      >
+        <Image
+          image={about.attributes.image}
+          className="inline-block mx-auto mb-10 sm:m-0 rounded-3xl"
+          alt="a picture of me"
+        />
 
-          <ReactMarkdown className="mt-10" rehypePlugins={[rehypeRaw]}>
+        <div className="inline-block [&>*>a]:text-orange-400 dark:[&>*>a]:text-cyan-400">
+          <h1 className="text-2xl sm:text-4xl mb-5">
+            {about.attributes.header}
+          </h1>
+          <ReactMarkdown
+            className="leading-relaxed 
+          "
+            rehypePlugins={[rehypeRaw]}
+          >
             {about.attributes.body}
           </ReactMarkdown>
 
+          <div id="currentPosition" className="mt-3">
+            Currently a {about.attributes.currentPosition.positionTitle} with{" "}
+            <Link href={about.attributes.currentPosition.companyLink}>
+              {about.attributes.currentPosition.companyTitle}
+            </Link>
+            .
+          </div>
+
           <ul className="list-none mt-5 [&>*]:inline-block space-x-5">
             <li>
-              <Link href={about.attributes.github} className="mr-6">
-                <FaGithubAlt className="mx-auto" size="30" />
+              <Link href={about.attributes.github}>
+                <FaGithubAlt size="30" />
               </Link>
             </li>
             <li>
-              <Link href={about.attributes.linkedIn} className="mr-6">
-                <FaLinkedin className="mx-auto" size="30" />
+              <Link href={about.attributes.linkedIn}>
+                <FaLinkedin size="30" />
+              </Link>
+            </li>
+            <li>
+              <Link href={about.attributes.resumeUrl}>
+                <FaFileWord size="30" />
               </Link>
             </li>
           </ul>
         </div>
-      </Layout>
-    );
+      </div>
+
+      <div id="skills" className="max-w-5xl m-auto text-center">
+        <h1 className="text-5xl bg-black text-white dark:bg-white dark:text-black p-5 my-5">Skills</h1>
+        <ul id="skillsList" className="list-none">
+          {Object.entries(about.attributes.skills).map((skill) => {
+            console.log(skill);
+
+            return (
+              <li className="mb-4" key={skill[0]}>
+                <h1 className="text-3xl inline-block mb-2">{skill[0]}</h1>
+                <p className="text-xl">{skill[1]}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Layout>
+  );
 };
 
 export async function getStaticProps() {
   // Run API calls in parallel
   const [aboutRes, categoriesRes] = await Promise.all([
-    fetchAPI("/about", { populate: "image" }),
+    fetchAPI("/about", { populate: {
+      image: "*",
+      currentPosition: "*",
+      skills: "*"
+    } }),
     fetchAPI("/categories", {}),
   ]);
 
