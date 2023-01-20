fb5adbb4681134e023ba3f8a5b0bf37b161aef32
Devin Sipal <devin@devinsipal.com>
2023-01-20 00:25:04 +0000

    make skillsSection smaller (max-w-3xl) remove hr

--- components/skillsSection.js
+++ components/skillsSection.js
@@ -1,17 +1,16 @@
 const SkillsSection = ({skills}) => {
     return(
-        <div id="skills" className="max-w-5xl m-auto p-4">
+        <div id="skills" className="max-w-3xl m-auto p-5">
         <h1 className="text-5xl bg-black text-white 
         dark:bg-white dark:text-black p-5 mt-5
         rounded-t-3xl text-center">What I can do for you</h1>
         <ul id="skillsList" className="list-none outline outline-1 mx-[1px] mb-0">
           {Object.entries(skills).map((skill) => {
             return (
-              <li className="mb-4" key={skill[0]}>
+              <li className="mb-4 p-2" key={skill[0]}>
                 <h1 className="text-3xl inline-block ml-5 mt-5 mb-3 
                 font-bold underline text-left">{skill[0]}:</h1>
                 <p className="text-xl mb-5 text-center">{skill[1]}</p>
-                <hr />
               </li>
               
             );
