8f82eb234454dca313b658e739999334f646ce2e
devin <devin@devinsipal.com>
2023-07-06 21:48:48 -0400

    update hero - center text better, color overlay

--- components/hero.js
+++ components/hero.js
@@ -5,9 +5,9 @@ export const Hero = ({title, image}) => {
     return (
       <div
         id="banner"
-        className="h-[40vh] mb-5 block text-white"
+        className="h-[25vh] bg-slate-600 bg-cover bg-blend-soft-light mb-5 block text-white flex items-center justify-center"
         style={{backgroundImage: "url(" + image + ")"}}>
-        <h1 className="text-center text-5xl font-bold drop-shadow-lg pt-5">
+        <h1 className="grid text-center text-5xl font-bold drop-shadow-lg pt-5">
           {title}
         </h1>
       </div>
