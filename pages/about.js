f309372cd04f793d2348a58aab63e4f6a00f8444
Devin Sipal <devin@devinsipal.com>
2024-05-18 16:46:46 -0400

    fix site for current schema

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Layout from "../components/layout";
-import { fetchAPI } from "../lib/api";
-import AboutSection from "../components/aboutSection";
-import SkillsSection from "../components/skillsSection";
+import React from "react"
+import Layout from "../components/layout"
+import { fetchAPI } from "../lib/api"
+import AboutSection from "../components/aboutSection"
+import SkillsSection from "../components/skillsSection"
 import TechSection from "../components/techSection"
-import Seo from "../components/seo";
+import Seo from "../components/seo"
 
 const About = ({ about, categories }) => {
   //TODO: set this up to get pageTitle from strapi somehow.
   const seo = {
-    pageTitle: "about"
+    pageTitle: "about",
   }
   return (
     <Layout categories={categories}>
@@ -18,25 +18,21 @@ const About = ({ about, categories }) => {
       {/* <SkillsSection skills={about.attributes.skills} /> */}
       {/* <TechSection tech={about.attributes.tech} /> */}
     </Layout>
-  );
-};
+  )
+}
 
 export async function getStaticProps() {
   // Run API calls in parallel
   const [aboutRes, categoriesRes] = await Promise.all([
-    fetchAPI("/about", { populate: {
-      image: "*",
-      currentPosition: "*",
-      skills: {
-        populate: "*"
+    fetchAPI("/about", {
+      populate: {
+        image: "*",
+        currentPosition: "*",
+        currentlyWorkingOn: "*",
       },
-      tech: {
-        populate: "*"
-      },
-      currentlyWorkingOn: "*"
-    } }),
+    }),
     fetchAPI("/categories", {}),
-  ]);
+  ])
 
   return {
     props: {
@@ -44,7 +40,7 @@ export async function getStaticProps() {
       categories: categoriesRes.data,
     },
     revalidate: 1,
-  };
+  }
 }
 
-export default About;
+export default About
